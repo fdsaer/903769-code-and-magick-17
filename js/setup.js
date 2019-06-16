fcfc8d65fedcf6e1ccd255320a2e765ee9d2be9f
Vladimir <vladimirstyopin@mail.ru>
2019-06-16 17:35:53 +0300

    Чистит код

--- js/setup.js
+++ js/setup.js
@@ -8,24 +8,22 @@ var NUMBER_OF_SIMILAR_WIZARDS = 4;
 
 var wizardForm = document.querySelector('.setup');
 var wizardList = wizardForm.querySelector('.setup-similar-list');
-var docPart = document.createDocumentFragment();
+var documentFragment = document.createDocumentFragment();
 var wizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
 var getRandomItem = function (arr) {
   var randomNumber = Math.floor(Math.random() * arr.length);
-  var item = arr[randomNumber];
-  return (item);
+  return arr[randomNumber];
 };
 
 var getRandomWizard = function () {
-  var randomWizard = {};
   var randomNumber = Math.floor(Math.random() * 2);
   var firstPart = getRandomItem(WIZARD_NAMES);
   var secondPart = getRandomItem(WIZARD_SURNAMES);
-  randomWizard.name = randomNumber ? (firstPart + ' ' + secondPart) : (secondPart + ' ' + firstPart);
-  randomWizard.coatColor = getRandomItem(COAT_COLORS);
-  randomWizard.eyesColor = getRandomItem(EYES_COLORS);
-  return (randomWizard);
+  return {'name': [randomNumber ? (firstPart + ' ' + secondPart) : (secondPart + ' ' + firstPart)],
+    'coatColor': [getRandomItem(COAT_COLORS)],
+    'eyesColor': [getRandomItem(EYES_COLORS)]
+  };
 };
 
 var getWizardCard = function () {
@@ -34,13 +32,13 @@ var getWizardCard = function () {
   wizardCard.querySelector('.setup-similar-label').textContent = wizard.name;
   wizardCard.querySelector('.wizard-coat').style.fill = wizard.coatColor;
   wizardCard.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
-  return (wizardCard);
+  return wizardCard;
 };
 
 for (var i = 0; i < NUMBER_OF_SIMILAR_WIZARDS; i++) {
-  docPart.appendChild(getWizardCard());
+  documentFragment.appendChild(getWizardCard());
 }
 
 wizardForm.classList.remove('hidden');
-wizardList.appendChild(docPart);
+wizardList.appendChild(documentFragment);
 wizardForm.querySelector('.setup-similar').classList.remove('hidden');
